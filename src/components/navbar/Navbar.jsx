5b520fddaa8731f406805e063b56f339163f8ebe
Rockn26 <irfanank26@gmail.com>
2024-03-28 14:56:26 +0300

    create Navbar Section with component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+
+    document.addEventListener('DOMContentLoaded', function () {
+        // Hamburger menü butonu seçiliyor
+        var hamburger = document.querySelector('.hamburger');
+        // Navigasyon bağlantılarını tutan konteynır seçiliyor
+        var navLinks = document.querySelector('.nav-right');
+     
+        // Hamburger menü butonuna tıklama olayı ekleniyor
+        hamburger.addEventListener('click', function () {
+            navLinks.classList.toggle('active');
+        // Hamburger butonuna 'active' sınıfını ekle/kaldır
+        this.classList.toggle('active');
+        });
+     });
+
+
+  return (
+    <div>
+        <nav class="navbar">
+            <a href="/" class="nav-logo">
+                <img src="assets/logo.png" alt="logo" />
+            </a>
+            <button class="hamburger" aria-label="Toggle navigation">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+            <div class="nav-right" id="nav-links">
+                <a href="#hero">Home</a>
+                <a href="#classes">Classes</a>
+                <a href="#trainers">Trainer</a>
+                <a href="#review">Review</a>
+                <a href="#contact">Contact</a>
+                <button class="join-us">JOIN US</button>
+            </div>
+        </nav>
+      
+    </div>
+  )
+}
+
+export default Navbar
