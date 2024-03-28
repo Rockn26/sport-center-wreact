adc57a3e0416a15717be1937870d0b15a322c1b8
Rockn26 <irfanank26@gmail.com>
2024-03-28 15:00:34 +0300

    create hero section

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Hero.style.css'
+
+const Hero = () => {
+  return (
+    <div>
+        <section class="hero" id="hero">
+        <div class="hero-text">
+            <h4>POWERFULL</h4>
+            <h2>GROUP</h2>
+            <h2>PRACTICE</h2>
+            <h2>WITH TRAINER</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt culpa suscipit inventore nisi
+                perferendis
+                nam corrupti, provident doloremque architecto. Quam alias asperiores debitis sunt ad labore nulla,
+                voluptatum totam amet?
+                In obcaecati cumque sit velit atque voluptatum non deleniti, facere nisi ratione, dolores aut
+                dignissimos,
+                ut inventore. Porro fuga eveniet consequatur eligendi quos dicta. Perspiciatis vero commodi labore
+                magnam
+                molestiae?</p>
+            <div class="hero-button">
+                <button class="button-colorfull">
+                    Sign Up
+                </button>
+                <button class="button">
+                    Details
+                </button>
+            </div>
+        </div>
+    </section>
+      
+    </div>
+  )
+}
+
+export default Hero
